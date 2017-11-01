fd8ff5197520ea633dad51f0b5385d8fb1c7594a
jswanborough <33280767+jswanborough@users.noreply.github.com>
2017-11-01 13:58:19 +0000

    Update tests.ts

--- tests.ts
+++ tests.ts
@@ -1,6 +1,6 @@
 {
     // Drive 1000 ms forward
-    invent.motor(InventMotor.all, MotorDirection.forwards, 100);
+    invent.motor(InventMotor.all, MotorDirection.forward, 100);
     basic.pause(1000);
 
     // Drive 1000 ms reverse
@@ -8,7 +8,7 @@
     basic.pause(1000);
 
     // Drive 1000 ms forward on left and reverse on right
-    invent.motor(InventMotor.left, MotorDirection.forwards, 100);
+    invent.motor(InventMotor.left, MotorDirection.forward, 100);
     invent.motor(InventMotor.right, MotorDirection.reverse, 100);
     basic.pause(1000);
 }
