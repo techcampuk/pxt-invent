ace69ef688cf249db0e56d343c7d8ba06678861d
jswanborough <33280767+jswanborough@users.noreply.github.com>
2018-05-18 11:17:05 +0100

    Line sensor blocks initial commit
    Added line sensor calibrate, digital read and analog read blocks

--- main.ts
+++ main.ts
@@ -18,6 +18,13 @@ enum InventMotorDir {
   Reverse
 }
 
+enum InventLineSensor {
+  //% block="left (P1)"
+  Left,
+  //% block="right(P0)"
+  Right
+}
+
 /**
  * Custom blocks
  */
@@ -75,4 +82,131 @@ namespace invent {
             }
         }
     }
+    
+    /** Line sensor global variables
+    */
+    
+    let item = 0
+    let time = 0
+    let r_min = 0
+    let l_min = 0
+    let r_max = 0
+    let l_max = 0
+    let l_thr = 0
+    let r_thr = 0
+    
+    /**
+      * Calibrate line sensors on P1 (left) and P0 (right)
+      */
+    //% blockId="invent_calibrate_line_sensors" block="calibrate line sensors"
+    //% weight=100
+    export function calibrate_line_sensors(): void {
+      l_max = 0
+      r_max = 0
+      l_min = 1023
+      r_min = 1023
+      invent.motor(InventMotor.Left, InventMotorDir.Reverse, 50)
+      invent.motor(InventMotor.Right, InventMotorDir.Forward, 50)
+      time = input.runningTime()
+      while (input.runningTime() - time < 1000) {
+          item = pins.analogReadPin(AnalogPin.P1)
+          if (item > l_max) {
+              l_max = item
+          }
+          if (item < l_min) {
+              l_min = item
+          }
+          item = pins.analogReadPin(AnalogPin.P0)
+          if (item > r_max) {
+              r_max = item
+          }
+          if (item < r_min) {
+              r_min = item
+          }
+      }
+      invent.motor(InventMotor.Left, InventMotorDir.Forward, 50)
+      invent.motor(InventMotor.Right, InventMotorDir.Reverse, 50)
+      time = input.runningTime()
+      while (input.runningTime() - time < 2000) {
+          item = pins.analogReadPin(AnalogPin.P1)
+          if (item > l_max) {
+              l_max = item
+          }
+          if (item < l_min) {
+              l_min = item
+          }
+          item = pins.analogReadPin(AnalogPin.P0)
+          if (item > r_max) {
+              r_max = item
+          }
+          if (item < r_min) {
+              r_min = item
+          }
+      }
+      l_thr = (l_max + l_min) / 2
+      r_thr = (r_max + r_min) / 2
+      invent.motor(InventMotor.Left, InventMotorDir.Reverse, 100)
+      invent.motor(InventMotor.Right, InventMotorDir.Forward, 100)
+      basic.pause(400)
+      while (pins.analogReadPin(AnalogPin.P0) > r_thr && pins.analogReadPin(AnalogPin.P1) > l_thr) {
+          invent.motor(InventMotor.Left, InventMotorDir.Reverse, 40)
+          invent.motor(InventMotor.Right, InventMotorDir.Forward, 40)
+      }
+    
+    }
+    
+    /**
+      * Digital Read Line Sensors
+      *
+      * @param sensor sensor to read
+      */
+    //% blockId="invent_line_sensor_digital" block="digital read line sensor %sensor"
+    //% weight=100
+    export function line_sensor_digital(sensor: InventLineSensor): int {
+      if (sensor == InventLineSensor.Left){  
+       if(pins.analogReadPin(AnalogPin.P1)>l_thr){
+         return 1
+       }
+       else {
+        return 0
+       }
+      } 
+      else {
+       if(pins.analogReadPin(AnalogPin.P0)>r_thr){
+         return 1
+       }
+       else {
+        return 0
+       }
+      }
+     
+    /**
+      * Analog Read Line Sensors
+      *
+      * @param sensor sensor to read
+      */
+    //% blockId="invent_line_sensor_analog" block="analog read line sensor %sensor"
+    //% weight=100
+    export function line_sensor_analog(sensor: InventLineSensor): int {
+      if (sensor == InventLineSensor.Left){  
+       l_anl = pins.map(
+         pins.analogReadPin(AnalogPin.P1),
+         l_min,
+         l_max,
+         0,
+         100
+         )
+       return l_anl
+      } 
+      else {
+       r_anl = pins.map(
+         pins.analogReadPin(AnalogPin.P0),
+         r_min,
+         r_max,
+         0,
+         100
+         )
+       return r_anl
+      }
 }
+    
